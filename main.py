530ac4f9db2463e43ac7f350a054be981914ce7c
Stuart Hoye <hoye@cs.ualberta.ca>
2017-05-11 14:29:55 -0600

    Add work before leaving.

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import classroom_manager as cm
+from classroom_manager import Manager
 import moss
+import json
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 
 # Trigger collection after deadline
 # http://apscheduler.readthedocs.io/en/latest/modules/triggers/date.html
 
+def rel():
+    print "RELEASE SIMULATION "
+def col():
+    print "COLLECTION SIMULATION "
+
 def main():
     try:
         org_name = "GitHubClassroomTestCMPUT229"
-        hub = cm.login()
-        org = cm.get_org(hub, org_name)
-        cm.set_teams()
-        cm.set_git_teams(hub, org)
-        cm.set_repos(org)
+        m = Manager(org_name)
+        m.set_teams()
+        m.set_git_teams()
+        m.set_repos("testlab1")
         try:
             moss.get_repos("testlab1", cm.get_teams(org))
             # moss.submit_repos("testlab1", cm.get_teams(org)["instructor"]) # To submit with base code
@@ -23,15 +28,28 @@ def main():
         moss.clear("testlab1")
     except:
         pass
-    cm.del_repos(org)
-    cm.del_teams(org)
+    m.del_repos()
+    m.del_teams()
 
     job.remove()
     sched.shutdown()
 
 if __name__ == "__main__":
+    f = open("./class/deadlines.json", "r")
+    deadlines = json.load(f)
     sched = BackgroundScheduler()
     sched.start()
-    job = sched.add_job(main, "date", run_date=datetime(2017, 5, 11, 13, 35))
-    while True:
-        pass
+    jobs = []
+
+    for lab in deadlines:
+        for t in deadlines[lab]:
+            if t == "release":
+                job = sched.add_job(rel, "date", run_date=deadlines[lab][t])
+                jobs.append(job)
+            elif t == "collect":
+                job = sched.add_job(col, "date", run_date=deadlines[lab][t])
+                jobs.append(job)
+
+
+    #job = sched.add_job(main, "date", run_date=datetime(2017, 5, 11, 14, 11))
+
